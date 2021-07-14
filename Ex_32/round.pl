42e19f023658b9f00e4132f7c5cd730c81653a20
SeekerRook <xrist.vardakis@gmail.com>
2021-07-14 18:09:48 +0300

    finished Prolog of Ex_32

--- Ex_32/round.pl
+++ Ex_32/round.pl
@@ -1,36 +1,9 @@
-% cars_of_city ([Inhead|Intail],T,[Outhead|Outtail],Iterator).
+%helpful predicates----
 is_equal(A,A).
-rot_increment(L,I,R):-
-    I < L-1 -> R is I+1; R is I - L + 1.
-
 maximum(A,B,Maxn):- A >= B -> Maxn is A; Maxn is B.
 minimum(A,B,Min):- A >= B -> Min is B; Min is A.
 first([Head|_],Head).
-% cars_of_city([Head],T,Head,Count,Res,R):-
-%         print("1"),
-%         Countn is Count+1,
-%         append(Res,[Countn],R). 
-% cars_of_city([Head],T,I,Count,Res,R):-
-%         print("2"),
-%         % is_equal(Head,I) -> cars_of_city([Head],T,Head,Prev,Count,Res,R);
-%         append(Res,[0],R).    
-% cars_of_city([Head|[Head|Tail]],T,Head,Count,Res,R):-
-%     print("3"),
-%     Countn is Count + 1,
-%     cars_of_city([Head|Tail],T,Head,Countn,Res,R).   
-% cars_of_city([Head|[Next|Tail]],T,Head,Count,Res,R):-
-%     print("4"),
-%     Countn is Count+1,
-%     append(Res,[Countn],NR),
-%     Nex is Head+1,
-%     cars_of_city([Next|Tail],T,Nex,0,NR,R).
-% cars_of_city([Head|[Next|Tail]],T,I,Count,Res,R):-
-%     print("5"),
-%     % is_equal(Head,I) -> cars_of_city([Head|[Next|Tail]],T,Head,Prev,Count,Res,R)
-%     % ; 
-%    append(Res,[0],NR),
-%     Nex is I+1,
-%     cars_of_city([Next|Tail],T,Nex,0,NR,R).
+
 
 zeros(N,L):-
     (N =\= 0 ->     Nn is N-1,
@@ -39,18 +12,21 @@ zeros(N,L):-
     ;   L = []
     ).
 
+%convert array of town per carr to cars in town----
 cars_of_city([], Iter,Count,T, Res):-
     Rest is T - Iter-1,
     zeros(Rest, Zeros),
     Res = [Count| Zeros].
-
 cars_of_city([Head|Tail], Iter,Count,T, Res):-
     (Head =:= Iter -> Countn is Count + 1,
     cars_of_city(Tail, Iter, Countn,T, Res);
     Countn is Iter +1,
     cars_of_city([Head|Tail],Countn ,0 ,T, Resn) ,
     Res =  [Count | Resn]).
-
+%help function to rotate pointer in a pac-man style rotation-----
+rot_increment(L,I,R):-
+    I < L-1 -> R is I+1; R is I - L + 1.
+%find next non zero town using a pac-man style rotation-----
 next_non_zero(_,I,1,_,I).    
 next_non_zero(List,I,0,T,R):-
     rot_increment(T,I,In),
@@ -59,6 +35,7 @@ next_non_zero(List,I,0,T,R):-
     rot_increment(T,I,In),
     next_non_zero(List,In,1,T,R).
 
+%compute max and sum for town 0-----
 sum_from_zero([],_,_,_,Sum,Sum).% Use without the first head
 sum_from_zero([Hs|Ts],T,I,Max,Sum,Sumr):-
     maximum(Hs,Max,Maxn),
@@ -68,24 +45,30 @@ sum_from_zero([Hs|Ts],T,I,Max,Sum,Sumr):-
     Sumn is Sum),
     sum_from_zero(Ts,T,In,Maxn,Sumn,Sumr).
 
+%find diff-----
 find_diff(Main,Sec,T,Diff):-
        ((Main>=Sec)->
         Diff is Main-Sec;
         Diff is T - Sec + Main).
+
+%check validity (same as ml)-----
 valid(Sum,Diff):-
     (Sum + 1) >= (2*Diff) .
 
+%find min town-----
 search(_,T,_,Min,Mint,T,_,_,Min,Mint).
-
 search(C,T,Lis,Min,Mint,Main,Sec,Sum,X,Y):-
+%  here we use 2 pointers (variables that store indexes not actual pointers) 
+% the main goes through the list once and the sec keeps the max (next non zero value)   
     
-    (is_equal(Main,Sec)-> next_non_zero(Lis,Sec,0,T,Secn); Secn is Sec),
+    (is_equal(Main,Sec)-> next_non_zero(Lis,Sec,0,T,Secn); Secn is Sec),%if main == sec update sec
     
     find_diff(Main,Secn,T,Diff),
 
     nth0(Main,Lis,Nth),
-    Sumn is Sum + C - (T * Nth ),
-    Mainn is Main+1,
+    Sumn is Sum + C - (T * Nth ),%calculate sum
+    Mainn is Main+1,%move main pointer
+
     (valid(Sumn,Diff) -> 
             (Min > Sumn->
                 search(C,T,Lis,Sumn,Main,Mainn,Secn,Sumn,X,Y)
@@ -111,7 +94,7 @@ read_line(Stream, L) :-
     atom_codes(Atom, Line),
     atomic_list_concat(Atoms, ' ', Atom),
     maplist(atom_number, Atoms, L).
-%----
+
 round(File,X,Y):-
     read_input(File,T,C,Lis),
     sort(0,@=<,Lis,Lissorted),
